163294b90885c725bfd271f46ecbe613ca9fd89f
yogamahastya <mahastyayoga97@gmail.com>
2025-08-29 13:31:52 +0700

    update

--- application/update_action.php
+++ application/update_action.php
@@ -63,7 +63,7 @@ if ($localVersion === $remoteVersion) {
 chdir($repoPath);
 
 // === JALANKAN GIT PULL ===
-$command = "sudo -u root git pull 2>&1";
+$command = "git pull 2>&1";
 $output  = shell_exec($command);
 
 // === SIMPAN LOG ===
